6389f5b3cae12704f3f1692cc5e80360979a6f61
Emiliano <verde_natural2003@hotmail.com>
2025-04-28 17:18:21 -0500

    ups

--- parser.c
+++ parser.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *filename = "";
+char *filename = "WSS_2025-04-28T2043.json";
 
 // objetivo: al ingresarle un número "n", imprimir todos los  atributos del registro n 
 
 int main(){
 
   FILE* archivo = fopen(filename, "r");
+  char buffer[1000];
 
+  
   if(archivo == NULL){
     printf("Error!\n");
+    fclose(archivo);
     return -1;
   }
 
+  int leidos_correctamente = fread(buffer, sizeof(char) * 50,50, archivo);
+
+  for(int i = 0; i < leidos_correctamente; i++){
+    printf("%c", buffer[i]);
+  }
   
+  printf("\n");
 
   
   fclose(archivo);
